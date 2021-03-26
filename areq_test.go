517f999970fb3f9825cad7c7035182faa1d93855
Gon <29622355+gonyyi@users.noreply.github.com>
2021-03-26 16:14:55 -0500

    Update areq_test.go

--- areq_test.go
+++ areq_test.go
@@ -2,17 +2,11 @@ package areq_test
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/gonyyi/areq"
 	"github.com/gonyyi/areq/option"
 	"testing"
 )
 
 func TestRequest_Req(t *testing.T) {
-	var out bytes.Buffer
-
-	if err := areq.Req("GET", "https://gonyyi.com/copyright", option.GetResBody(&out)); err != nil {
-		println(err)
-	}
-
-	println(out.String())
 }
