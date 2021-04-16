e0c8e8ab3328c90fe12fb2a5a8a5fa8d096b252b
Gon <29622355+gonyyi@users.noreply.github.com>
2021-04-16 16:43:28 -0500

    Update areq.go

--- areq.go
+++ areq.go
@@ -11,17 +11,6 @@ import (
 	"time"
 )
 
-var globalReq = NewRequest()
-
-func Req(method string, url string, options ...*Option) error {
-	return globalReq.Req(method, url, options...)
-}
-
-type Option struct {
-	Req   func(*http.Request) error
-	Cli   func(cli *http.Client) error
-	Res   func(r *http.Response) error
-}
 
 type Request struct {
 	Client    *http.Client
@@ -50,23 +39,26 @@ func NewRequest() *Request {
 	return &r
 }
 
-func (r *Request) Req(method string, url string, options ...*Option) error {
+func (r *Request) Req(method string, url string, options ...*DoFn) error {
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		return err
 	}
+	req.Header.Set("User-Agent", VERSION)
 	// if close the connection after the request such when there are tons of diff servers to send few req.
 	// req.Close = true
 
 	for _, v := range options {
-		if v.Req != nil {
-			if err := v.Req(req); err != nil {
-				return err
+		if v != nil {
+			if v.Req != nil {
+				if err := v.Req(req); err != nil {
+					return err
+				}
 			}
-		}
-		if v.Cli != nil {
-			if err := v.Cli(r.Client); err != nil {
-				return err
+			if v.Cli != nil {
+				if err := v.Cli(r.Client); err != nil {
+					return err
+				}
 			}
 		}
 	}
@@ -81,9 +73,11 @@ func (r *Request) Req(method string, url string, options ...*Option) error {
 	}
 
 	for _, v := range options {
-		if v.Res != nil {
-			if err := v.Res(res); err != nil {
-				return err
+		if v != nil {
+			if v.Res != nil {
+				if err := v.Res(res); err != nil {
+					return err
+				}
 			}
 		}
 	}
