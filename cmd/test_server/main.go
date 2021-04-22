d9d1160f1217dc7e04ae8474b93a269ad9830438
Gon <29622355+gonyyi@users.noreply.github.com>
2021-04-22 11:19:15 -0500

    Update main.go

--- cmd/test_server/main.go
+++ cmd/test_server/main.go
@@ -6,21 +6,30 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"os"
+	"strconv"
 )
 
 func main() {
 	log := alog.New(os.Stderr).SetFormatter(ext.NewFormatterTerminalColor())
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+	var result string
+	http.HandleFunc("/result", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte(result))
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		out, err := httputil.DumpRequest(r, false)
 		log.Info().Str("url", r.RequestURI).Write("")
-		if err!=nil {
+		if err != nil {
+			w.WriteHeader(400)
 			log.Error().Err("error", err).Write("")
+			result = err.Error()
+			w.Write([]byte(`{"status":"err","err":` + strconv.Quote(err.Error()) + `}`))
 		} else {
-			println("------DUMP BEGIN------")
-			println(string(out)+"\n")
+			result = string(out)
+			w.WriteHeader(200)
+			w.Write([]byte(`{"status":"ok"}`))
+			println(result+"\n")
 		}
-		w.Write([]byte(`{"status":"ok"}`))
 	})
 
 	http.ListenAndServe(":8080", nil)
