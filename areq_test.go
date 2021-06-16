6bfacffee88a18c7823a8dcf47d8b4737c439b0a
Gon <29622355+gonyyi@users.noreply.github.com>
2021-06-16 10:57:37 -0500

    Update areq_test.go

--- areq_test.go
+++ areq_test.go
@@ -2,5 +2,12 @@ package areq_test
 
 import "testing"
 
-func TestRequest_Req(t *testing.T) {
+func TestRequest_ReqBody(t *testing.T) {
+	b := "hello world"
+	out := bytes.Buffer{}
+	// var out []byte
+	err := areq.Req("POST", "https://httpbin.org/post", areq.Do.ReqBody([]byte(b)), areq.Do.ResTo(&out))
+	if err != nil {println(err.Error())}
+	println("ok")
+	println(out.String())
 }
