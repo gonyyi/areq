cfd4e741cd0c4df08c164e03725db7234227ebb6
Gon <29622355+gonyyi@users.noreply.github.com>
2021-06-16 17:46:23 -0500

    Update areq.go

--- areq.go
+++ areq.go
@@ -4,290 +4,86 @@
 package areq
 
 import (
-	"bytes"
-	"compress/gzip"
-	"encoding/json"
+	"crypto/tls"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
-	"strings"
+	"time"
 )
 
-type DoFn struct {
-	Name string
-	Req  func(*http.Request) error
-	Cli  func(cli *http.Client) error
-	Res  func(r *http.Response) error
+const VERSION = "areq/0.2.0"
+
+type Request struct {
+	Client    *http.Client
+	Transport *http.Transport
 }
 
-var Do dofns
-
-type dofns struct{}
-
-func (dofns) If(condition bool, do *DoFn) *DoFn {
-	if condition {
-		return do
-	}
-	return nil
-}
-
-func (dofns) Join(do ...*DoFn) *DoFn {
-	var dofn DoFn
-	var dofnNames []string
-	var req []func(*http.Request) error
-	var cli []func(cli *http.Client) error
-	var res []func(r *http.Response) error
-	for _, f := range do {
-		if f.Name != "" {
-			dofnNames = append(dofnNames, f.Name)
-		} else {
-			dofnNames = append(dofnNames, "NONAME-DOFN")
-		}
-		if f.Req != nil {
-			req = append(req, f.Req)
-		}
-		if f.Cli != nil {
-			cli = append(cli, f.Cli)
-		}
-		if f.Res != nil {
-			res = append(res, f.Res)
-		}
-	}
-	dofn.Req = func(r *http.Request) error {
-		for _, f := range req {
-			if err := f(r); err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-	dofn.Cli = func(c *http.Client) error {
-		for _, f := range cli {
-			if err := f(c); err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-	dofn.Res = func(r *http.Response) error {
-		for _, f := range res {
-			if err := f(r); err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-	dofn.Name = "DoJoin(" + strings.Join(dofnNames, ",") + ")"
-	return &dofn
-}
-
-func (dofns) AuthBasic(id, pwd string) *DoFn {
-	return &DoFn{
-		Name: "AuthBasic",
-		Req: func(req *http.Request) error {
-			req.SetBasicAuth(id, pwd)
-			return nil
+func NewRequest() *Request {
+	r := Request{}
+	r.Transport = &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
 		},
+		DialContext: (&net.Dialer{
+			Timeout:   2 * time.Second,
+			KeepAlive: 600 * time.Second,
+		}).DialContext,
+		// ForceAttemptHTTP2:     true,
+		DisableKeepAlives:   false,
+		MaxIdleConns:        100,
+		MaxIdleConnsPerHost: 100,
 	}
+	r.Client = &http.Client{
+		Timeout:   time.Second * 10,
+		Transport: r.Transport,
+	}
+	return &r
 }
 
-func (dofns) UseCookieJar(jar http.CookieJar) *DoFn {
-	return &DoFn{
-		Name: "UseCookieJar",
-		Cli: func(cli *http.Client) error {
-			cli.Jar = jar
-			return nil
-		},
+func (r Request) Req(method string, url string, options ...*DoFn) error {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return err
 	}
-}
+	req.Header.Set("User-Agent", VERSION)
+	// if close the connection after the request such when there are tons of diff servers to send few req.
+	// req.Close = true
 
-func (dofns) AddCookie(k, v string) *DoFn {
-	return &DoFn{
-		Name: "AddCookie",
-		Req: func(req *http.Request) error {
-			req.AddCookie(&http.Cookie{Name: k, Value: v})
-			return nil
-		},
-	}
-}
-
-func (dofns) AddCookies(cookies []*http.Cookie) *DoFn {
-	return &DoFn{
-		Name: "AddCookies",
-		Req: func(req *http.Request) error {
-			for i := 0; i < len(cookies); i++ {
-				req.AddCookie(cookies[i])
-			}
-			return nil
-		},
-	}
-}
-
-func (dofns) ReqFunc(f func(r *http.Request) error) *DoFn {
-	return &DoFn{
-		Name: "ReqFunc",
-		Req:  f,
-	}
-}
-
-func (dofns) ReqBodyStr(body string) *DoFn {
-	return &DoFn{
-		Name: "ReqBodyStr",
-		Req: func(r *http.Request) error {
-			r.Body = ioutil.NopCloser(strings.NewReader(body))
-			return nil
-		},
-	}
-}
-
-func (dofns) ReqBody(body io.Reader) *DoFn {
-	return &DoFn{
-		Name: "ReqBody",
-		Req: func(r *http.Request) error {
-			r.Body = ioutil.NopCloser(body)
-			return nil
-		},
-	}
-}
-
-func (dofns) ReqBodyBytes(body []byte) *DoFn {
-	return &DoFn{
-		Name: "ReqBodyBytes",
-		Req: func(r *http.Request) error {
-			r.Body = ioutil.NopCloser(bytes.NewReader(body))
-			return nil
-		},
-	}
-}
-
-func (dofns) ReqHeader(h http.Header) *DoFn {
-	return &DoFn{
-		Name: "ReqHeader",
-		Req: func(r *http.Request) error {
-			r.Header = h
-			return nil
-		},
-	}
-}
-
-func (dofns) ReqHeaderAdd(k, v string) *DoFn {
-	return &DoFn{
-		Name: "ReqHeaderAdd",
-		Req: func(r *http.Request) error {
-			r.Header.Add(k, v)
-			return nil
-		},
-	}
-}
-
-func (dofns) ReqHeaderSet(k, v string) *DoFn {
-	return &DoFn{
-		Name: "ReqHeaderSet",
-		Req: func(r *http.Request) error {
-			r.Header.Set(k, v)
-			return nil
-		},
-	}
-}
-
-func (dofns) ResFunc(f func(*http.Response) error) *DoFn {
-	return &DoFn{
-		Name: "ResFunc",
-		Res:  f,
-	}
-}
-
-func (dofns) GetCookie(name string, dst *string) *DoFn {
-	return &DoFn{
-		Name: "GetCookie",
-		Res: func(r *http.Response) error {
-			for _, v := range r.Cookies() {
-				if v.Name == name {
-					*dst = v.Value
-				}
-			}
-			return nil
-		},
-	}
-}
-
-func (dofns) GetCookies(dst []*http.Cookie) *DoFn {
-	return &DoFn{
-		Name: "GetCookies",
-		Res: func(r *http.Response) error {
-			dst = r.Cookies()
-			return nil
-		},
-	}
-}
-
-func (dofns) ResJSONTo(dst interface{}) *DoFn {
-	return &DoFn{
-		Name: "ResJSONTo",
-		Res: func(res *http.Response) error {
-			var dec *json.Decoder
-			switch res.Header.Get("Content-Encoding") {
-			case "gzip":
-				tmp, err := gzip.NewReader(res.Body)
-				if err != nil {
-					return err
-				}
-				defer tmp.Close()
-				dec = json.NewDecoder(tmp)
-			default:
-				dec = json.NewDecoder(res.Body)
-			}
-
-			// maybe need dec.Token() before dec.More().. not sure when..
-			for dec.More() {
-				err := dec.Decode(dst)
-				if err != nil {
+	for _, v := range options {
+		if v != nil {
+			if v.Req != nil {
+				if err := v.Req(req); err != nil {
 					return err
 				}
 			}
-			return nil
-		},
-	}
-}
-
-func (dofns) ResTo(dst io.Writer) *DoFn {
-	return &DoFn{
-		Name: "ResTo",
-		Res: func(res *http.Response) error {
-			_, err := io.Copy(dst, res.Body)
-			return err
-		},
-	}
-}
-
-func (dofns) ResGzipTo(dst io.Writer) *DoFn {
-	return &DoFn{
-		Name: "ResGzipTo",
-		Req: func(req *http.Request) error {
-			req.Header.Add("Accept-Encoding", "gzip")
-			return nil
-		},
-		Res: func(res *http.Response) error {
-			// println("encoding: ", res.Header.Get("Content-Encoding"))
-			switch res.Header.Get("Content-Encoding") {
-			case "gzip":
-				tmp, err := gzip.NewReader(res.Body)
-				if err != nil {
-					return err
-				}
-				defer tmp.Close()
-
-				_, err = io.Copy(dst, tmp)
-				if err != nil {
-					return err
-				}
-			default:
-				_, err := io.Copy(dst, res.Body)
-				if err != nil {
+			if v.Cli != nil {
+				if err := v.Cli(r.Client); err != nil {
 					return err
 				}
 			}
-			return nil
-		},
+		}
 	}
+
+	res, err := r.Client.Do(req)
+	// if body.Close is after the error check, when redirect, res won't be nil.
+	if res != nil {
+		defer res.Body.Close()
+	}
+	if err != nil {
+		return err
+	}
+
+	for _, v := range options {
+		if v != nil {
+			if v.Res != nil {
+				if err := v.Res(res); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	_, err = io.Copy(ioutil.Discard, res.Body)
+	return err
 }
