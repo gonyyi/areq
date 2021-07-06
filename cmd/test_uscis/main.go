b087df9eb760f08a9ee398e7b5567572064fddd8
Gon <29622355+gonyyi@users.noreply.github.com>
2021-07-06 14:48:35 -0500

    Update main.go

--- cmd/test_uscis/main.go
+++ cmd/test_uscis/main.go
@@ -6,13 +6,8 @@ import (
 	"time"
 )
 
-// curl 'https://egov.uscis.gov/processing-times/api/processingtime/N-400/MEM' \
-//  -H 'Connection: keep-alive' \
-//  -H 'Accept: application/json, text/plain, */*' \
-//  -H 'Referer: https://egov.uscis.gov/processing-times/' \
-//  -H 'Accept-Language: en-US,en;q=0.9' \
 func main() {
-	err, a, b := check()
+	err, a, b := CheckN400Status()
 	if err != nil {
 		println(err.Error())
 		return
@@ -24,57 +19,38 @@ const (
 	uscisMemphis = "https://egov.uscis.gov/processing-times/api/processingtime/N-400/MEM"
 )
 
-type resp struct {
-	Data struct {
-		ProcessingTime struct {
-			FormInfoEn string      `json:"form_info_en"`
-			FormInfoEs string      `json:"form_info_es"`
-			FormName   string      `json:"form_name"` // "N-400"
-			FormNoteEn interface{} `json:"form_note_en"`
-			FormNoteEs interface{} `json:"form_note_es"`
-			OfficeCode string      `json:"office_code"`
-			Range      []struct {
-				// {"unit":"Months","unit_en":"Months","unit_es":"Meses","value":14}
-				// {"unit":"Months","unit_en":"Months","unit_es":"Meses","value":8}
-				Unit   string `json:"unit"`
-				UnitEn string `json:"unit_en"`
-				Value  int    `json:"value"`
-			} `json:"range"`
-			Subtypes []struct {
-				FormType        string `json:"form_type"`
-				PublicationDate string `json:"publication_date"` // "June 23, 2021"
-				Range           []struct {
+
+func CheckN400Status() (err error, low string, high string) {
+	// curl 'https://egov.uscis.gov/processing-times/api/processingtime/N-400/MEM' \
+	//  -H 'Connection: keep-alive' \
+	//  -H 'Accept: application/json, text/plain, */*' \
+	//  -H 'Referer: https://egov.uscis.gov/processing-times/' \
+	//  -H 'Accept-Language: en-US,en;q=0.9' \
+
+	type resp struct {
+		Data struct {
+			ProcessingTime struct {
+				FormName   string      `json:"form_name"` // "N-400"
+				Range      []struct {
+					// {"unit":"Months","unit_en":"Months","unit_es":"Meses","value":14}
+					// {"unit":"Months","unit_en":"Months","unit_es":"Meses","value":8}
 					Unit   string `json:"unit"`
-					UnitEn string `json:"unit_en"`
-					UnitEs string `json:"unit_es"`
 					Value  int    `json:"value"`
 				} `json:"range"`
-				ServiceRequestDate   string      `json:"service_request_date"`
-				ServiceRequestDateEn string      `json:"service_request_date_en"`
-				ServiceRequestDateEs string      `json:"service_request_date_es"`
-				SubtypeInfoEn        string      `json:"subtype_info_en"`
-				SubtypeInfoEs        string      `json:"subtype_info_es"`
-				SubtypeNoteEn        interface{} `json:"subtype_note_en"`
-				SubtypeNoteEs        interface{} `json:"subtype_note_es"`
-			} `json:"subtypes"`
-		} `json:"processing_time"`
-	} `json:"data"`
-	Message string `json:"message"`
-}
+			} `json:"processing_time"`
+		} `json:"data"`
+	}
 
-func check() (err error, low string, high string) {
 	var out resp
 
-	req := areq.NewRequest()
-	req.SetDialTimeout(15*time.Second, 60*time.Second)
+	req := areq.NewRequest().SetTimeout(15*time.Second, -1, -1, -1)
 
 	err = req.Req("GET", uscisMemphis,
-		areq.Do.SetClientTimeout(15*time.Second),
 		areq.Do.ReqHeaderAdd("Accept", "application/json"),
 		areq.Do.ReqHeaderAdd("Referer", "https://egov.uscis.gov/processing-times/"),
 		areq.Do.ResJSONTo(&out),
-
 	)
+
 	if err != nil {
 		return err, "", ""
 	}
