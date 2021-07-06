45c0e7b8abe30c0a4796778f4e7de4f426d4b4f0
Gon <29622355+gonyyi@users.noreply.github.com>
2021-07-06 14:47:34 -0500

    Update areq.go

--- areq.go
+++ areq.go
@@ -12,40 +12,75 @@ import (
 	"time"
 )
 
-const VERSION = "areq/0.2.0"
+const (
+	VERSION                 = "areq/0.2.2"
+	DefaultDialTimeout      = 5 * time.Second
+	DefaultDialKeepAlive    = 600 * time.Second
+	DefaultHandshakeTimeout = 5 * time.Second
+	DefaultClientTimeout    = 10 * time.Second
+)
 
 type Request struct {
 	Client    *http.Client
 	Transport *http.Transport
 }
 
-func (r *Request) SetDialTimeout(timeout, keepAlive time.Duration) {
-	r.Transport.DialContext = (&net.Dialer{
-		Timeout:   timeout,
-		KeepAlive: keepAlive,
-	}).DialContext
-}
-
-func NewRequest() *Request {
-	r := Request{}
+func (r Request) Init() Request {
 	r.Transport = &http.Transport{
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: true,
 		},
 		DialContext: (&net.Dialer{
-			Timeout:   5 * time.Second,
-			KeepAlive: 600 * time.Second,
+			Timeout:   DefaultDialTimeout,
+			KeepAlive: DefaultDialKeepAlive,
 		}).DialContext,
 		// ForceAttemptHTTP2:     true,
+		TLSHandshakeTimeout: DefaultHandshakeTimeout,
 		DisableKeepAlives:   false,
 		MaxIdleConns:        100,
 		MaxIdleConnsPerHost: 100,
 	}
 	r.Client = &http.Client{
-		Timeout:   time.Second * 10,
+		Timeout:   DefaultClientTimeout,
 		Transport: r.Transport,
 	}
-	return &r
+	return r
+}
+
+// SetTimeout will set various timeout setting of request.
+// If set to -1, it will use default value.
+// If set to 0, it means timeout will not be used.
+func (r Request) SetTimeout(dialTimeout, dialKeepAlive, handshakeTimeout, clientTimeout time.Duration) Request {
+	if dialTimeout == -1 {
+		dialTimeout = DefaultDialTimeout
+	}
+	if dialKeepAlive == -1 {
+		dialKeepAlive = DefaultDialKeepAlive
+	}
+	if handshakeTimeout == -1 {
+		handshakeTimeout = DefaultHandshakeTimeout
+	}
+	if clientTimeout == -1 {
+		clientTimeout = DefaultClientTimeout
+	}
+
+	if r.Transport == nil || r.Client == nil {
+		r = r.Init()
+	}
+
+	r.Transport.DialContext = (&net.Dialer{
+		Timeout:   dialTimeout,
+		KeepAlive: dialKeepAlive,
+	}).DialContext
+	r.Transport.TLSHandshakeTimeout = handshakeTimeout
+
+	r.Client.Timeout = clientTimeout
+
+	return r
+}
+
+func NewRequest() Request {
+	return Request{}.Init()
 }
 
 func (r Request) Req(method string, url string, options ...*DoFn) error {
