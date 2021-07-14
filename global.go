a7e5c7e388c707598c73aa50c8583728ba77e38d
Gon <29622355+gonyyi@users.noreply.github.com>
2021-07-14 16:59:56 -0500

    Update global.go

--- global.go
+++ global.go
@@ -1,7 +1,10 @@
 // (c) 2021 Gon Y Yi. 
 // https://gonyyi.com
+// 07/14/21 Wed 16:59
 
 package areq
+
+import "time"
 
 var globalReq = NewRequest()
 
@@ -17,3 +20,30 @@ func (e Err) Error() string {
 	return string(e)
 }
 
+// DefaultSetting will let the user change the default behavior
+var DefaultSetting = defaults{
+	DialTimeout:         5 * time.Second,
+	DialKeepAlive:       600 * time.Second,
+	HandshakeTimeout:    5 * time.Second,
+	ClientTimeout:       10 * time.Second,
+	DisableKeepAlives:   false,
+	MaxIdleConns:        100,
+	MaxIdleConnsPerHost: 100,
+}
+
+type defaults struct {
+	DialTimeout           time.Duration
+	DialKeepAlive         time.Duration
+	HandshakeTimeout      time.Duration
+	ClientTimeout         time.Duration
+	TLSInsecureSkipVerify bool
+	ForceAttemptHTTP2     bool
+	DisableKeepAlives     bool
+	MaxIdleConns          int
+	MaxIdleConnsPerHost   int
+}
+
+// UpdateGlobal will update the current default setting into global Req method
+func (defaults) UpdateGlobal() {
+	globalReq = globalReq.Init()
+}
